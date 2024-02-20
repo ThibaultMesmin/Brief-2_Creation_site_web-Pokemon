51b89551e6dd7ed805e1197aac2ccccf2176c7db
marietcht <marie.tranchant@maif.fr>
2024-02-20 15:04:35 +0100

    rangement pages et dossiers

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ var burgerMenu = document.getElementById('burger-menu');
 var overlay = document.getElementById('menu');
 
 burgerMenu.addEventListener('click', function() {
-  this.classList.toggle("close");
-  overlay.classList.toggle("overlay");
+    console.log(this);
+    this.classList.toggle("close");
+    overlay.classList.toggle("overlay");
 });
+
+// links = ["/", "pages/pokedex.html", "pages/collections.html", "pages/contact.html"];
+// let nav = document.createElement('nav');
+// let ul = document.createElement('ul');
+
+// document.querySelector('header').innerHTML = nav.append(ul)

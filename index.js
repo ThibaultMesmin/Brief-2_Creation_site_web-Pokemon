f837d5d662d77cb9dfac51a04fe08dabf0aee5c2
marietcht <marie.tranchant@maif.fr>
2024-02-20 15:20:17 +0100

    corrige conflit

--- index.js
+++ index.js
@@ -13,3 +13,27 @@ burgerMenu.addEventListener('click', function() {
 // let ul = document.createElement('ul');
 
 // document.querySelector('header').innerHTML = nav.append(ul)
+
+// window.addEventListener('load', () => {
+//     document.querySelector('header').innerHTML = `
+//     <img src="images/logo-pokemon2.png" alt="logo-pokemon" id="logoNavBar">
+//         <nav>
+//             <a href="index.html">Accueil</a>
+//             <a href="pages/pokedex.html">Pokedex</a>
+//             <a href="pages/collections.html">Collections</a>
+//             <a href="pages/contact.html">Contact</a>
+//         </nav>
+
+//         <div id="burger-menu">
+//             <span></span>
+//         </div>
+    
+//         <div id="menu">
+//             <ul>
+//               <li><a href="index.html">Accueil</a></li>
+//               <li><a href="pages/pokedex.html">Pokedex</a></li>
+//               <li><a href="pages/collections.html">Collections</a></li>
+//               <li><a href="pages/contact.html">Contact</a></li>
+//             </ul>
+//         </div>`
+// })

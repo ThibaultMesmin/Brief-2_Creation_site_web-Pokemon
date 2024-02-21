52f781c575827cf4fdd062521715a1e7d5af92b8
ThibaultMesmin <thibault.mesmin@maif.fr>
2024-02-21 09:55:06 +0100

    début journée 2

--- index.js
+++ index.js
@@ -25,6 +25,21 @@ window.addEventListener('load', () => {
         </div>`
 })
 
+// évènement ajouté au chargement de la page avec ajout direct d'html dans le DOM (footer logo réseaux sociaux)
+window.addEventListener('load', () => {
+    document.querySelector('footer').innerHTML += `
+    <section class="footer">
+        <div class="reseau">
+            <i class="fa-brands fa-instagram"></i>
+            <i class="fa-brands fa-facebook"></i>
+            <i class="fa-brands fa-twitter"></i>
+            <i class="fa-brands fa-youtube"></i>
+        </div>
+        <div id="site-by">
+        </div>
+    </section>`
+})
+
 // setTimeOut pour que mon code puisse être lu après le chargement de la page
 setTimeout(() => {
 

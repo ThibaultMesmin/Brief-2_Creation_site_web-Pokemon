dff39a0abf72c893c8c974d4f54b2556ae65b568
ThibaultMesmin <thibault.mesmin@maif.fr>
2024-02-21 13:55:35 +0100

    update carousel fonctionnel

--- index.js
+++ index.js
@@ -37,12 +37,13 @@ window.addEventListener('load', () => {
         </div>
         <div id="site-by">
         </div>
-    </section>`
-})
+    </section>`;
+});
+
+// Reste du code...
 
 // setTimeOut pour que mon code puisse être lu après le chargement de la page
 setTimeout(() => {
-
     // récupère les éléments injectés du DOM
     let burgerMenu = document.getElementById('burger-menu');
     let overlay = document.getElementById('menu');
@@ -61,3 +62,42 @@ let button = document.querySelector('button');
 // button.onclick = () => {
 //     window.open('');
 //   };
+
+// ####### CAROUSEL ########
+
+let items = document.querySelectorAll('.slider .item');
+    let next = document.getElementById('next');
+    let prev = document.getElementById('prev');
+    
+    let active = 3;
+    function loadShow(){
+        let stt = 0;
+        items[active].style.transform = `none`;
+        items[active].style.zIndex = 1;
+        items[active].style.filter = 'none';
+        items[active].style.opacity = 1;
+        for(var i = active + 1; i < items.length; i++){
+            stt++;
+            items[i].style.transform = `translateX(${120*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(-1deg)`;
+            items[i].style.zIndex = -stt;
+            items[i].style.filter = 'blur(5px)';
+            items[i].style.opacity = stt > 2 ? 0 : 0.6;
+        }
+        stt = 0;
+        for(var i = active - 1; i >= 0; i--){
+            stt++;
+            items[i].style.transform = `translateX(${-120*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(1deg)`;
+            items[i].style.zIndex = -stt;
+            items[i].style.filter = 'blur(5px)';
+            items[i].style.opacity = stt > 2 ? 0 : 0.6;
+        }
+    }
+    loadShow();
+    next.onclick = function(){
+        active = active + 1 < items.length ? active + 1 : active;
+        loadShow();
+    }
+    prev.onclick = function(){
+        active = active - 1 >= 0 ? active - 1 : active;
+        loadShow();
+    }

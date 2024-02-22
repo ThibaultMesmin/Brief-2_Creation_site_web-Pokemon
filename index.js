b4db182bb1b9ebc51af7e227121d369a09baef33
ThibaultMesmin <thibault.mesmin@maif.fr>
2024-02-22 15:13:22 +0100

    merci marie pour ce push

--- index.js
+++ index.js
@@ -28,20 +28,20 @@ window.addEventListener('load', () => {
     // toujours dans l'évènement de chargement de page, ajout du carousel dans la page collections.html
 
     let items =[
-        "collection_manga/1.jpg", 
-        "collection_manga/2.jpg",
-        "collection_manga/3.jpg",
-        "collection_manga/4.jpg",
-        "collection_manga/5.jpg",
-        "collection_manga/6.jpg",
-        "collection_manga/7.jpg",
-        "collection_manga/8.jpg",
-        "collection_manga/9.jpg",
-        "collection_manga/10.jpg",
-        "collection_manga/11.jpg",
-        "collection_manga/12.jpg",
-        "collection_manga/13.jpg",
-        "collection_manga/14.jpg",
+        "1.jpg", 
+        "2.jpg",
+        "3.jpg",
+        "4.jpg",
+        "5.jpg",
+        "6.jpg",
+        "7.jpg",
+        "8.jpg",
+        "9.jpg",
+        "10.jpg",
+        "11.jpg",
+        "12.jpg",
+        "13.jpg",
+        "14.jpg",
     ];
 
     let next = document.getElementById('next');
